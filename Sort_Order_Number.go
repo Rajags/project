ca2e07d9288d092e63feb03db53cb0ee8d9e9ca2
Rajags <rajags89@gmail.com>
2020-06-06 01:42:20 +0530

    Create Sort_Order_Number.go

---
+++ Sort_Order_Number.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("before sort array of values ")
+	numbers := []int{2,3,4,6,1}
+	fmt.Println(numbers)
+	 
+	SortOrderNumber(numbers)
+}
+
+func SortOrderNumber(numbers []int){
+	for i :=1;i<len(numbers);i++{
+	   if numbers[i]<numbers[i-1]{
+	   numbers[i] = numbers[i] + numbers[i-1]
+	   numbers[i-1] =numbers[i]-numbers[i-1]
+	   numbers[i] =numbers[i]-numbers[i-1]
+	   i=0
+	   }
+	}
+	fmt.Println("after sorting asc array of values ",numbers)
+}
